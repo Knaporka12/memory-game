242f73d2eb1ce4c126402267064ae5147cb468ff
Kacper <you@example.com>
2023-12-21 18:14:52 +0100

    sprawdzanie referencji na img

--- playground/playground.js
+++ playground/playground.js
@@ -7,23 +7,37 @@
 
 // console.log(tab[0].src)
 
-const a = {
-    name: 'kacper',
-    age: 19
-}
-let b;
- b = Object.assign({}, a);
+// const a = {
+//     name: 'kacper',
+//     age: 19
+// }
+// let b;
+//  b = Object.assign({}, a);
 
-console.log(a === b);
-console.log(a)
-console.log(b)
+// console.log(a === b);
+// console.log(a)
+// console.log(b)
 
-const image = document.querySelector(`.js-img`);
+// const image = document.querySelector(`.js-img`);
 
-let target = {};
+// let target = {};
 
-target = Object.assign({}, image);
+// target = Object.assign({}, image);
 
-console.log(image)
-console.log(target)
+// console.log(image)
+// console.log(target)
+
+const image = document.querySelectorAll(`.js-img`);
+
+console.log(typeof image[0])
+
+//console.log(image[1] === image[0]);
+
+image[0] = image[1];
+image[0].classList.remove(`js-img`)
+console.log(image[0].classList)
+console.log(image[1].classList)
+
+console.log(image[1] == image[0]);
+
 
