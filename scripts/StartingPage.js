414eebd24cef596c8f444682157deffdfa6a5492
Kacper <you@example.com>
2023-12-29 12:34:22 +0100

    article i button do zmiany wersji

--- scripts/StartingPage.js
+++ scripts/StartingPage.js
@@ -1,20 +1,25 @@
 export function renderStartingPage(){
 
-    let winsCount = JSON.parse(localStorage.getItem('winsCount'));
+    let currentVersion = JSON.parse(localStorage.getItem('currentVersion'));
+    if (!currentVersion) currentVersion = 'pudzian';
+
+    const winsCount = JSON.parse(localStorage.getItem('winsCount'));
     if (!winsCount) winsCount = 0;
 
     const mainEle = document.querySelector(`.js-main`);
 
     const html = ` 
 
-            <h2>
-                Your Wins: ${winsCount}
-            </h2>
+            <article>
+                <h2>
+                    Your Wins: ${winsCount}
+                </h2>
 
-            <p class = "unlock-count-p">Win ${10 - winsCount} more games to unlock premium version!</p>
-            <p class = "choose-diff-p">Choose difficulty:</p>
-
-            <section>
+                <p class = "unlock-count-p">Win ${10 - winsCount} more games to unlock premium version!</p>
+                <button class = "toggle-version-btn js-toggle-version-btn"></button>
+                <p class = "choose-diff-p">Choose difficulty:</p>
+            </article>
+            <section>  
 
                 <button class = "js-difficulty-button"data-level="easy">Easy</button>
                 <button class = "js-difficulty-button"data-level="medium">Medium</button>
@@ -23,7 +28,12 @@ export function renderStartingPage(){
             </section>
 
     `
-
     mainEle.innerHTML = html;
+
+    const versionBtn = document.querySelector(`.js-toggle-version-btn`);
+
+    if (winsCount >= 10) versionBtn.classList.add('visible-display');
+    currentVersion === 'pudzian' ? versionBtn.innerHTML = 'Enable Premium version' : versionBtn.innerHTML = 'Disable premium version';
+    
 }
 
